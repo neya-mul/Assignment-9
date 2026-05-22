522a9ada2c86bf4e569f789ed91735b9d6aeb705
neya-mul <neyamulislam946@gmail.com>
2026-05-22 15:44:22 +0600

    search  functionality added

--- src/app/all-pets/page.jsx
+++ src/app/all-pets/page.jsx
@@ -3,18 +3,19 @@ import PetCard from '@/components/cardComponents/PetCard'
 import React, { useEffect, useState } from 'react'
 
 export default function AllPets() {
-const [pets, setPets] = useState([])
+  const [pets, setPets] = useState([])
+  const [name, setName] = useState('')
 
-useEffect(()=>{
-  fetch('http://localhost:5000/pets')
-  .then(res=> res.json())
-  .then(data=> setPets(data))
 
-},[])
+  useEffect(() => {
+    fetch(`http://localhost:5000/pets?searchName=${name}`)
+      .then(res => res.json())
+      .then(data => setPets(data))
+
+  }, [name])
 
   // const res = await fetch('http://localhost:5000/pets')
   // const pets = await res.json()
-  const [name, setName] = useState('')
 
 
   const search = (e) => {
@@ -23,12 +24,12 @@ useEffect(()=>{
   }
   console.log(name);
   console.log(pets);
-  
 
-// This converts both names to lowercase and checks if the search string is inside the pet's name
-const expectedPet = pets.filter(p => 
-  p.petName.toLowerCase().includes(name.toLowerCase())
-)  
+
+  // // This converts both names to lowercase and checks if the search string is inside the pet's name
+  // const expectedPet = pets.filter(p =>
+  //   p.petName.toLowerCase().includes(name.toLowerCase())
+  // )
 
   return (
     <div className='mt-30 text-black min-h-screen'>
@@ -39,15 +40,17 @@ const expectedPet = pets.filter(p =>
         <div className='flex justify-center mt-6'>
           <input
             onChange={search}
+            value={name}
             type='text'
             placeholder='Search pets...'
             className='w-1/2 p-3 text-black bg-white border border-gray-300 rounded-lg outline-none focus:outline-none focus:border-blue-400 focus:ring-2 focus:ring-blue-100' />
         </div>
       </div>
 
+     
       <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
         {
-          expectedPet.map((pet, ind) => (
+          pets.map((pet, ind) => (
             <PetCard pet={pet} key={ind}></PetCard>
           ))
         }
